815e630dc705d8262834e71701f2f075e5b74c3c
ananya-singh7 <ananyasinghhh777@gmail.com>
2022-06-22 17:10:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/foodmenuapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.foodmenuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
